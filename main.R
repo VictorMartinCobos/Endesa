28e5cde13c76fbe7d4f51f30e0140a40f3095f14
victormartincobos <victormartincobos@gmail.com>
2021-03-22 23:45:15 +0100

    Initial commit. Created functions to get input directory and list of each file in directory. Map function to get csv list. Reduce function to get an unified tibble.

---
+++ main.R
@@ -0,0 +1,29 @@
+# ENDESA REPORT GENERATOR MAIN ----------------------------------------------
+library(tidyverse)
+library(lubridate)
+source("utils.R")
+
+#Select a Directory
+directory_path <- rstudioapi::selectDirectory(
+  caption = "Select Directory",
+  label = "Select",
+  path = "~/Documents/"
+)
+
+#Obtain a list of all csv contained in directory chosen
+list_csv_names <- list.files(
+  path = directory_path,
+  pattern = "*.csv",
+  all.files = TRUE,
+  full.names = TRUE
+)
+
+#Import each csv
+csv_list <- map(list_csv_names, import_csv)
+
+#Join csv in an unique tibble
+data <- reduce(csv_list, bind_rows)
+
+
+
+
