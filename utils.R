cc11e3fbbf4a32010a6e14c026176fcf66b2687e
victormartincobos <victormartincobos@gmail.com>
2021-03-25 23:28:29 +0100

    Adding functions to calculate summarise data.

--- utils.R
+++ utils.R
@@ -22,38 +22,50 @@ tidy_csv <- function(file) {
     transmute(
       date = Fecha,
       hour = hm(str_sub(Hora, 1, 5)),
-      consume = `Consumo (Wh)`) %>%
+      consumption = `Consumo (Wh)`
+    ) %>%
     add_count(date) %>% 
     transmute(
-      date,
-      hour,
-      consume,
-      ts = case_when(
-        n == 25 & hour == "2H 0M" ~ as_datetime(
-          str_c(date, hour - hm("1H 0M"), sep = " "),
-          tz = "Europe/Madrid",
-          format = "%Y-%m-%d %HH %MM %SS")
-        + dhours(),
-        n == 25 & hour > "2H 0M" ~ as_datetime(
-          str_c(date, hour - hm("1H 0M"), sep = " "),
-          tz = "Europe/Madrid",
-          format = "%Y-%m-%d %HH %MM %SS"),
-        n == 23 & hour >= "2H 0M" ~ as_datetime(
-          str_c(date, hour + hm("1H 0M"), sep = " "),
-          tz = "Europe/Madrid",
-          format = "%Y-%m-%d %HH %MM %SS"),
-        hour == "0s" ~ as_datetime(
-          str_c(date, "0H", sep = " "),
-          tz = "Europe/Madrid",
-          format = "%Y-%m-%d %HH"),
-        TRUE ~ as_datetime(
-          str_c(date, hour, sep = " "),
-          tz = "Europe/Madrid",
-          format = "%Y-%m-%d %HH %MM %SS")
-      ),
-      n)
-  }
-      
+      ts = parse(n, hour, date),
+      consumption
+    )
+}
 
-#25 horas -> CEST a CET. Las 2 se fuerzan a CEST. El resto se resta una hora.
-#23 horas -> CET a CEST
+#Parse function
+parse <- function(n, hour, date) {
+  case_when(
+    n == 25 & hour == "2H 0M" ~ as_datetime(
+      str_c(date, hour - hm("1H 0M"), sep = " "),
+      tz = "Europe/Madrid",
+      format = "%Y-%m-%d %HH %MM %SS")
+    + dhours(),
+    n == 25 & hour > "2H 0M" ~ as_datetime(
+      str_c(date, hour - hm("1H 0M"), sep = " "),
+      tz = "Europe/Madrid",
+      format = "%Y-%m-%d %HH %MM %SS"),
+    n == 23 & hour >= "2H 0M" ~ as_datetime(
+      str_c(date, hour + hm("1H 0M"), sep = " "),
+      tz = "Europe/Madrid",
+      format = "%Y-%m-%d %HH %MM %SS"),
+    hour == "0s" ~ as_datetime(
+      str_c(date, "0H", sep = " "),
+      tz = "Europe/Madrid",
+      format = "%Y-%m-%d %HH"),
+    TRUE ~ as_datetime(
+      str_c(date, hour, sep = " "),
+      tz = "Europe/Madrid",
+      format = "%Y-%m-%d %HH %MM %SS")
+  )
+}
+
+#Obtain total consumption
+total_consumption <- function(file) {
+  file %>%
+    summarise(mean(consumption))
+}
+
+#Obtain mean
+mean <- function(file, var) {
+  file %>% 
+    group_by(var)
+}
